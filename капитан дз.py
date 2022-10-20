697733c76b1e3683476cad85c2d49465e984bd1c
Giga <113104983+kolenka8@users.noreply.github.com>
2022-10-20 11:06:27 +0300

    Update homework 28.09

--- капитан дз.py
+++ капитан дз.py
@@ -1,7 +1,7 @@
 import datetime
-def captain(lst,dat):
-    with open('note.txt','w') as file:
+def captain(lst,dat): # Функция принимает текст (записи капитана) и дату записи
+    with open('note.txt','w') as file: # Создаём файл (Дневник капитана) 
         for days in range(len(lst)):
             file.writelines(["{0}:{1}{2}".format(datetime.datetime.strptime(dat,'%Y.%m.%d')+datetime.timedelta(days),lst[days],'\n')])
-
+            # с помощью strptime создаём объект datetime из строки и с помощью timedelta запоминаем разницу между двумя датами 
 captain(['Здравствуйте','Досвидания'],'2022.10.17')
