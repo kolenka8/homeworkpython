548ffc8db3304dd6074921078e49804e39569497
Giga <113104983+kolenka8@users.noreply.github.com>
2022-10-21 13:18:36 +0300

    Update homework 19.09

--- дз 19,09.py
+++ дз 19,09.py
@@ -1,18 +1,22 @@
+def prv_visokos(year): # Проверка на високосный год
+    if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
+        return False
+    else:
+        return True
 def proverka(year,month,day):
+    month_31 = (1,3,5,7,8,10,12)
+    month_30 = (4,6,9,11)
     if 0 < month < 13 and 0 < day < 32: #Ограничения по датам(макс дней, месяцев).
-        if year % 4 == 0 and year % 100 !=0 or year % 400 == 0: # Проверка на високосный год
-            if month == 2: # В високосном году в феврале 29 дней.
-                if day < 30:
-                    return True
-                else:
-                    return False
-        elif month == 2: # Но если год не високосный, то в феврале будет 28 дней.
-            if day < 29:
-                return True
-            else:
-                return False
-        elif year % 4 != 0 and month != 2: # В противном случае выводим True
+        if prv_visokos(year) == True and month == 2 and 0 < day < 30: # Если високосный год, то в феврале 29 дней
             return True
+        elif (prv_visokos(year) == True and month !=2 and 0 < day < 32) :
+            return True
+        elif prv_visokos(year) == False and month == 2 and 0 < day < 29: # Но если год не високосный, то в феврале будет 28 дней.
+            return True
+        elif (month in month_31 and 0 < day < 32) or (month in month_30 and 0 < day < 31): # Проверка дней по месяцам
+            return True
+        else:
+            return False
     else:
         return False
 year = int(input("Год: "))
